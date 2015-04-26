a25d8fdb709f9d346582da07560a487e9c7fa482
Anthony Prades <toony.github@chezouam.net>
2015-04-26 15:08:48 +0200

    Django 1.8: remove south dependency

--- rssgenerator/migrations/0001_initial.py
+++ rssgenerator/migrations/0001_initial.py
@@ -1,76 +1,47 @@
 # -*- coding: utf-8 -*-
-import datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from __future__ import unicode_literals
+
+from django.db import models, migrations
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        # Adding model 'Rss'
-        db.create_table(u'rssgenerator_rss', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('title', self.gf('django.db.models.fields.CharField')(max_length=256)),
-            ('link', self.gf('django.db.models.fields.URLField')(max_length=1024)),
-            ('description', self.gf('django.db.models.fields.CharField')(max_length=256)),
-        ))
-        db.send_create_signal(u'rssgenerator', ['Rss'])
+    dependencies = [
+    ]
 
-        # Adding model 'Items'
-        db.create_table(u'rssgenerator_items', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('rss', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['rssgenerator.Rss'])),
-            ('title', self.gf('django.db.models.fields.CharField')(max_length=256)),
-            ('link', self.gf('django.db.models.fields.URLField')(max_length=1024)),
-            ('pub_date', self.gf('django.db.models.fields.DateTimeField')()),
-            ('summary', self.gf('django.db.models.fields.TextField')(null=True, blank=True)),
-        ))
-        db.send_create_signal(u'rssgenerator', ['Items'])
-
-        # Adding model 'Links'
-        db.create_table(u'rssgenerator_links', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('item', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['rssgenerator.Items'])),
-            ('link', self.gf('django.db.models.fields.URLField')(max_length=1024)),
-        ))
-        db.send_create_signal(u'rssgenerator', ['Links'])
-
-
-    def backwards(self, orm):
-        # Deleting model 'Rss'
-        db.delete_table(u'rssgenerator_rss')
-
-        # Deleting model 'Items'
-        db.delete_table(u'rssgenerator_items')
-
-        # Deleting model 'Links'
-        db.delete_table(u'rssgenerator_links')
-
-
-    models = {
-        u'rssgenerator.items': {
-            'Meta': {'object_name': 'Items'},
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'link': ('django.db.models.fields.URLField', [], {'max_length': '1024'}),
-            'pub_date': ('django.db.models.fields.DateTimeField', [], {}),
-            'rss': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['rssgenerator.Rss']"}),
-            'summary': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
-            'title': ('django.db.models.fields.CharField', [], {'max_length': '256'})
-        },
-        u'rssgenerator.links': {
-            'Meta': {'object_name': 'Links'},
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'item': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['rssgenerator.Items']"}),
-            'link': ('django.db.models.fields.URLField', [], {'max_length': '1024'})
-        },
-        u'rssgenerator.rss': {
-            'Meta': {'object_name': 'Rss'},
-            'description': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'link': ('django.db.models.fields.URLField', [], {'max_length': '1024'}),
-            'title': ('django.db.models.fields.CharField', [], {'max_length': '256'})
-        }
-    }
-
-    complete_apps = ['rssgenerator']
+    operations = [
+        migrations.CreateModel(
+            name='Items',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=256)),
+                ('link', models.URLField(max_length=1024)),
+                ('pub_date', models.DateTimeField(verbose_name=b'date published')),
+                ('summary', models.TextField(null=True, blank=True)),
+            ],
+            options={
+                'ordering': ['-pub_date'],
+            },
+        ),
+        migrations.CreateModel(
+            name='Links',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('link', models.URLField(max_length=1024)),
+                ('item', models.ForeignKey(to='rssgenerator.Items')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Rss',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=256)),
+                ('description', models.CharField(max_length=256)),
+            ],
+        ),
+        migrations.AddField(
+            model_name='items',
+            name='rss',
+            field=models.ForeignKey(to='rssgenerator.Rss'),
+        ),
+    ]
