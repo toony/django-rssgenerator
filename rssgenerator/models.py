92b1cf4183913c3c0d87d3388b049a0db5988b69
Anthony Prades <toony.github@chezouam.net>
2015-07-26 18:48:08 +0200

    Feature: download links mark as localy stored

--- rssgenerator/models.py
+++ rssgenerator/models.py
@@ -1,6 +1,11 @@
 from django.db import models
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+import os
+import urllib
+
 
 class Rss(models.Model):
 	title = models.CharField(max_length=256, null=False)
@@ -16,9 +21,36 @@ class Items(models.Model):
 	summary = models.TextField(null=True, blank=True)
 	class Meta:
 		ordering = ["-pub_date"]
+		
 	def __unicode__(self):
 		return self.title
 	
 class Links(models.Model):
 	item = models.ForeignKey(Items)
 	link = models.URLField(max_length=1024, null=False)
+	storeLocaly = models.BooleanField(default=True)
+	
+	def delete(self, *args, **kwargs):
+		super(Links, self).delete(*args, **kwargs)
+
+@receiver(post_save,sender=Links)
+def __storeLink(sender, instance, **kwargs):
+	link = instance
+	item = link.item
+	
+	storeDir = os.path.join(settings.RSSGENERATOR_LOCAL_DATA, str(item.rss.id))
+	storeDir = os.path.join(storeDir, str(item.id))
+	linkLocalName = os.path.join(storeDir, str(link.id))
+		
+	print str(link.id)+" "+str(link.storeLocaly)
+	if not link.storeLocaly:
+		if os.path.exists(linkLocalName):
+			print 'delete local'+str(link.id)
+			os.remove(linkLocalName)
+		return
+		
+	if not os.path.exists(storeDir):
+		os.makedirs(storeDir)
+			
+	linkDownloader = urllib.URLopener()
+	linkDownloader.retrieve(link.link, linkLocalName)
