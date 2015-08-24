52035bd1e3ee8d1e95e164d31003639976e61fb1
Anthony Prades <toony.github@chezouam.net>
2015-08-24 18:25:31 +0200

    Doc: build development environment

--- README.md
+++ README.md
@@ -2,6 +2,15 @@
 
 A very simple RSS generator created for my personal needs
 
+## Developer environment
+
+Build it using:
+```
+git clone https://github.com/toony/django-rssgenerator.git django-rssgenerator
+cd django-rssgenerator
+./buildDevEnv.sh
+```
+
 ## Docker
 Build docker container using: 
 ```
---
+++ buildDevEnv.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+which virtualenv > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+	echo "virtualenv must be installed and accessible from PATH"
+	exit 1
+fi
+
+WORKSPACE=`dirname $0`
+pushd ${WORKSPACE}
+WORKSPACE=`pwd`
+GIT_CLONE=`basename ${WORKSPACE}`
+
+PYENV="../rssgenerator-env"
+mkdir -p ${PYENV}
+pushd ${PYENV}
+
+# Create and activate virtualenv
+virtualenv rssgenerator-env
+. ./rssgenerator-env/bin/activate
+
+# Install dependencies
+pip install Django PyRSS2Gen python-magic
+
+# Initialize Django project
+django-admin startproject project
+
+popd
+cp "docker/djangoProjet-urls.py" ${PYENV}"/project/project/urls.py"
+
+pushd ${PYENV}"/project"
+ln -s "../../"${GIT_CLONE}"/rssgenerator" .
+
+pushd project
+sed -i -e "s/\(INSTALLED_APPS =.\+$\)/\1\n    'rssgenerator',/" settings.py
+popd
+
+python manage.py migrate
+
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin')" | python manage.py shell
+echo "Administrator account: admin/admin"
+
+exec python manage.py runserver
+
