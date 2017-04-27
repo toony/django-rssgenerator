0c220726dfd39ffd5c934a7379baadcfbf9b44c6
Anthony Prades <toony.github@chezouam.net>
2017-04-28 00:49:31 +0200

    Feat: add search URL

--- rssgenerator/urls.py
+++ rssgenerator/urls.py
@@ -16,6 +16,7 @@ urlpatterns = [
     url(r'^(?P<rss_id>\d+)/gallery$', 'rssgenerator.views.rssgallery', name='rssgallery'),
     url(r'^(?P<rss_id>\d+)/gallery/random$', 'rssgenerator.views.rssgalleryrandom', name='rssgalleryrandom'),
     url(r'^(?P<rss_id>\d+)/allItemsId$', 'rssgenerator.views.allitemsid', name='allitemsid'),
+    url(r'^(?P<rss_id>\d+)/search$', 'rssgenerator.views.searchItem', name='searchitem'),
     url(r'^(?P<rss_id>\d+)/(?P<item_id>\d+)/summary$', 'rssgenerator.views.itemsummary', name='itemsummary'),
     url(r'^(?P<rss_id>\d+)/(?P<item_id>\d+)/gallery$', 'rssgenerator.views.itemgallery', name='itemgallery'),
     url(r'^(?P<rss_id>\d+)/localStore/(?P<item_id>\d+)/(?P<link_id>\d+)/get$', 'rssgenerator.views.localstoreretrieve', name='localstoreretrieve')
