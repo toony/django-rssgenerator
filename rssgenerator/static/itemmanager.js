64da57497b632e7ea751b7efa3c27575d0db2bde
Anthony Prades <toony.github@chezouam.net>
2017-05-13 21:57:37 +0200

    Feat: add overlay to gallery item

--- rssgenerator/static/itemmanager.js
+++ rssgenerator/static/itemmanager.js
@@ -2,18 +2,35 @@ function Item(itemSummary) {
     this.itemSummary = itemSummary;
     
     this.thumb = function() {
-        var img = jQuery('<img/>')
+        return jQuery('<div/>')
+            .addClass('thumb-content')
+            .append(this.preview())
+            .append(this.menu());
+    }
+    
+    this.preview = function() {
+        return jQuery('<img/>')
+            .addClass('thumb-preview')
             .attr('id', 'picItem' + this.itemSummary['id']);
-        
-        return jQuery('<a/>')
-            .attr('href', '#')
-            .append(img);
+    }
+    
+    this.menu = function() {
+        var gallery = jQuery('<div/>')
+            .addClass('thumb-menu-item')
+            .addClass('fa')
+            .addClass('fa-play-circle-o')
+            .addClass('fa-2x')
+            .click(itemSummary['gallery'], displayGallery);
+
+        return jQuery('<div/>')
+            .addClass('thumb-menu')
+            .append(gallery);
     }
     
     this.itemElement = jQuery('<figure/>')
         .attr('id', 'item' + this.itemSummary['id'])
         .addClass('thumb')
         .css('opacity', '0')
-        .append(this.thumb())
-        .click(itemSummary['gallery'], displayGallery);
+        .append(this.thumb());
+//        .click(itemSummary['gallery'], displayGallery);
 };
