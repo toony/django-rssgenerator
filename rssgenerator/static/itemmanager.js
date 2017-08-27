e2cd46afc2ca0c1c4a0e30addcde0532eb78e30b
Anthony Prades <toony.github@chezouam.net>
2017-08-27 18:52:26 +0200

    Feat: add info icon

--- rssgenerator/static/itemmanager.js
+++ rssgenerator/static/itemmanager.js
@@ -1,5 +1,6 @@
 function Item(itemSummary) {
     this.itemSummary = itemSummary;
+    this.thumbPreview;
     
     this.thumb = function() {
         return jQuery('<div/>')
@@ -21,10 +22,22 @@ function Item(itemSummary) {
             .addClass('fa-play-circle-o')
             .addClass('fa-2x')
             .click(itemSummary['gallery'], displayGallery);
+            
+        var infos = jQuery('<div/>')
+            .addClass('thumb-menu-item')
+            .addClass('fa')
+            .addClass('fa-info-circle')
+            .addClass('fa-2x')
+            .click( {
+                        'itemId': 'picItem' + this.itemSummary['id'],
+                        'summary': itemSummary
+                    },
+                    displayItemInfos);
 
         return jQuery('<div/>')
             .addClass('thumb-menu')
-            .append(gallery);
+            .append(gallery)
+            .append(infos);
     }
     
     this.itemElement = jQuery('<figure/>')
@@ -33,3 +46,7 @@ function Item(itemSummary) {
         .css('opacity', '0')
         .append(this.thumb());
 };
+
+function displayItemInfos(event) {
+    console.log(event);
+}
