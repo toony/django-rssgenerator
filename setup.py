e43f732c701e789dd3007258e544ac428cd263a3
Anthony Prades <toony.github@chezouam.net>
2021-05-03 10:20:17 +0200

    Chore: version 1.9

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name = 'django-rssgenerator',
-    version = '1.8',
+    version = '1.9',
     packages = ['rssgenerator'],
     include_package_data = True,
     license = 'GNU GPLv3+',
